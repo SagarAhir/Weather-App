1f8b05187a4aceae8bcde89d315dd1fb7e34b926
Sagar Ahir <sagar1ahir@gmail.com>
2021-09-19 13:09:44 +0530

    "adde icons"

--- main/views.py
+++ main/views.py
@@ -8,12 +8,15 @@ def index(request):
     if request.method == 'POST':
         city = request.POST['city']
         
-        source = urllib.request.urlopen(
-            'http://api.openweathermap.org/data/2.5/weather?q=' + city + 'YourApiKey').read()
-
+        try:
+            source = urllib.request.urlopen(
+                'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=ec90b5cac62a2c858e6ce157b8b8799d').read()
+        except:
+            data = {'error': '404'}
+            return render(request, "main/index.html", data)
         json_data = json.loads(source)
         # print(json_data)
-
+        
         kelvin = json_data['main']['temp']
         celsius = round(kelvin - 273.15,2)
         data = {
@@ -25,7 +28,6 @@ def index(request):
             "wind_speed": str(json_data['wind']['speed']),
             "wind_degree": str(json_data['wind']['deg']),
         }
-
         # print(data)
     else:
         data = {}
